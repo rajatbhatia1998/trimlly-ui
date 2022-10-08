2d986169097409f5dc3d488913811c9fc0e77a3a
rajatbhatia1998 <rajat.bhatia859@gmail.com>
2022-10-08 11:22:02 +0530

    dashboard table responsive added

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -74,7 +74,7 @@ export default function Dashboard() {
       
 
 
-
+{!showDrawer && <div onClick={()=>{toggleSidebar()}} className='drawerBGTransaparent'></div>}
 <nav class="bg-white border-gray-200 px-1 sm:px-4 py-2.5 rounded dark:bg-gray-900">
   <div class="container flex flex-wrap justify-between items-center mx-auto">
     <div style={{cursor:'pointer'}} onClick={()=>{toggleSidebar()}}>
@@ -96,12 +96,12 @@ export default function Dashboard() {
 </nav>
 
 
-<div id="drawer-navigation" class=" hidden fixed z-40 h-screen p-4 overflow-y-auto bg-white w-80 dark:bg-gray-800" tabindex="-1" aria-labelledby="drawer-navigation-label">
+<div id="drawer-navigation" class="hidden fixed z-40 h-screen p-4 overflow-y-auto bg-white w-80 dark:bg-gray-800" tabindex="-1" aria-labelledby="drawer-navigation-label">
     <h5 id="drawer-navigation-label" class="text-base font-semibold text-gray-500 uppercase dark:text-gray-400">{user.displayName ? user.displayName :user.email}</h5>
-    {/* <button type="button" data-drawer-dismiss="drawer-navigation" aria-controls="drawer-navigation" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white">
+    <button onClick={()=>{toggleSidebar()}} type="button" data-drawer-dismiss="drawer-navigation" aria-controls="drawer-navigation" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white">
         <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
         <span class="sr-only">Close menu</span>
-    </button> */}
+    </button>
   <div class="py-4 overflow-y-auto">
       <ul class="space-y-2">
         
--- src/components/DashboardDefault.js
+++ src/components/DashboardDefault.js
@@ -11,6 +11,7 @@ import { getAuth,signOut} from 'firebase/auth'
 import {useSelector} from 'react-redux'
 import axios from 'axios';
 import {getFormatedDate,getRedirectUrl} from '../extras/commanScript'
+import './css/dashboard.css'
 const { Text, Link } = Typography;
 
 
@@ -62,9 +63,16 @@ export default function DashboardDefault() {
         const data = [
             {
               key: '1',
-              date: getFormatedDate("2022-09-29T10:07:11.328Z"),
+              date: "2022-09-29T10:07:11.328Z",
               clicks: 32,
               shortUrl: 'trimlly.com/NU_r-oAVJ',
+              longUrl: 'www.codifiedcoder.in',
+            },
+            {
+              key: '2',
+              date: "2022-09-28T10:07:11.328Z",
+              clicks: 23,
+              shortUrl: 'trimlly.com/NU_r-oAV',
               longUrl: 'www.google.com',
             },
           
@@ -73,7 +81,7 @@ export default function DashboardDefault() {
             setIsFetching(false)
             setUrlData(data)
         },5000)
-    })
+    },[])
       
   return (
     <div className="">
@@ -94,7 +102,67 @@ export default function DashboardDefault() {
               <PlusOutlined /> <span className='ml-1'>CREATE</span></button>
             
             </div>
-            <Table columns={columns} dataSource={urlData} />
+            
+<div class="overflow-x-auto relative shadow-md sm:rounded-lg mt-2">
+    
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="p-4">
+                    <div class="flex items-center">
+                      
+                    </div>
+                </th>
+                <th scope="col" class="py-3 px-6">
+                    Date
+                </th>
+                <th scope="col" class="py-3 px-6">
+                   Short Link
+                </th>
+                <th scope="col" class="py-3 px-6">
+                    Original Link
+                </th>
+                <th scope="col" class="py-3 px-6">
+                    Clicks
+                </th>
+                <th scope="col" class="py-3 px-6">
+                    Actions
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            
+            {urlData.map(url=>{
+              return <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+              <td class="p-4 w-4">
+                  <div class="flex items-center">
+                      <input id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                      <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
+                  </div>
+              </td>
+              <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              {getFormatedDate(url.date)}
+              </th>
+              <td class="py-4 px-6">
+              <Link style={{marginTop:10,marginBottom:10}} href={getRedirectUrl(url.shortUrl)} target="_blank">
+              <Text type='danger' strong copyable italic>{url.shortUrl}</Text>
+          </Link>
+              </td>
+              <td class="py-4 px-6">
+                  {url.longUrl}
+              </td>
+              <td class="py-4 px-6">
+                  {url.clicks}
+              </td>
+              <td class="py-4 px-6">
+              <EditFilled/>
+              </td>
+          </tr>
+            })}
+        </tbody>
+    </table>
+</div>
+
 
             </>
         }
